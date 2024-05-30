c297951439451157de285411541a618ecbf06b49
dst01 <dst01@DESKTOP-RCC9MKJ>
2024-05-30 14:47:30 +0900

    예약취소까지 작업물

---
+++ final/src/main/java/hotelReservation/dao/impl/reservation/PayDaoImpl.java
@@ -0,0 +1,79 @@
+package hotelReservation.dao.impl.reservation;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import hotelReservation.dao.reservation.PayDao;
+import hotelReservation.dto.CreatePid;
+import hotelReservation.dto.CustomerLogin;
+import hotelReservation.dto.HotelInfo;
+import hotelReservation.dto.Pay;
+import lombok.Getter;
+import lombok.Setter;
+
+@Repository
+@Getter
+@Setter
+public class PayDaoImpl implements PayDao {
+
+	@Autowired
+	private SqlSessionTemplate sqlSessionTemplate;
+	
+	
+	@Override
+	public String payCid(String cid) {
+		String rCid = sqlSessionTemplate.selectOne("pay.payCid", cid);
+		return rCid;
+	}
+
+	@Override
+	public String payLogin(CustomerLogin cl) {
+		String cpw = sqlSessionTemplate.selectOne("pay.payLogin", cl);
+		return cpw;
+	}
+
+	
+	@Override
+	public int priceInfo(String tcode) {
+		int price = sqlSessionTemplate.selectOne("pay.priceInfo", tcode);
+		return price;
+	}
+
+	@Override
+	public String createPid(CreatePid cp) {
+		String pid = sqlSessionTemplate.selectOne("pay.createPid", cp);
+		return pid;
+	}
+	
+	@Override
+	public int payInfo(Pay pay) {
+		int cnt = sqlSessionTemplate.insert("pay.payInfo", pay);
+		return cnt;
+	}
+
+
+	@Override
+	public HotelInfo completeInfo(String tcode) {
+		HotelInfo hInfo = sqlSessionTemplate.selectOne("pay.complete", tcode); 
+		return hInfo;
+	}
+	
+	@Override
+	public String cancelPid(String rid) {
+		String pid = sqlSessionTemplate.selectOne("pay.cancelPid", rid);
+		return pid;
+	}
+	
+	@Override
+	public int cancel(String pid) {
+		int cnt = sqlSessionTemplate.delete("pay.cancel", pid);
+		return cnt;
+	}
+
+
+
+
+
+}
+
---
+++ final/src/main/java/hotelReservation/dto/CreateRid.java
@@ -0,0 +1,21 @@
+package hotelReservation.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateRid {
+
+	private String country;
+	private String rdate;
+
+
+	public CreateRid(String country, String rdate) {
+		this.country = country;
+		this.rdate = rdate;
+
+	}
+	
+	
+}
